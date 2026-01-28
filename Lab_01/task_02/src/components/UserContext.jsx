6a94eeab2f9fb27d794b41d6667556e47911f945
LINESKL <priveth06@gmail.com>
2026-01-28 21:47:32 +0500

    add UserContext for user state management

---
+++ Lab_01/task_02/src/components/UserContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+// Создаем сам контекст
+const UserContext = createContext();
+
+// Кастомный хук для использования данных
+export default const useUser = () => {
+    const context = useContext(UserContext);
+
+    if (!context) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+
+    return context;
+}
